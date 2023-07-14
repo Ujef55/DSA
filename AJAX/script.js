874f025917f71aacab7fa7ae5d6f6a78c7dd688b
Ujef55 <ujef.kelawala123@gmail.com>
2023-07-14 20:45:22 +0530

    fixing javascript

--- AJAX/script.js
+++ AJAX/script.js
@@ -1,7 +1,10 @@
 const profileImage = document.querySelector('.profileImage');
 const followerSpan = document.querySelector('.followers');
 const followingSpan = document.querySelector('.following');
+const bioSpan = document.querySelector('.bio');
 const clickButton = document.querySelector('.card-button');
+const link = document.querySelector('.link');
+const cardDescription = document.querySelector('.card-desciption');
 
 const xhr = new XMLHttpRequest();
 const user = 'https://api.github.com/users/Ujef55';
@@ -13,10 +16,14 @@ xhr.onreadystatechange = function() {
         const profilePic = data.avatar_url;
         const followers = data.followers;
         const following = data.following;
+        const bio = data.bio;
         function showData() {
+            profileImage.setAttribute('src', profilePic);
+            bioSpan.textContent = `Bio - ${bio}`;
             followerSpan.textContent = `Followers is ${followers}`;
             followingSpan.textContent = `Following is ${following}`;
-            profileImage.setAttribute('src', profilePic);
+            link.style.display = 'block';
+            cardDescription.style.display = 'none';
         }
         clickButton.addEventListener('click', showData);
     }
