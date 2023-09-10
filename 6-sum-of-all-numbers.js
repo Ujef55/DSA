c7d908c5b427094dfc6875efd94e3954cc0d662c
Ujef55 <ujef.kelawala123@gmail.com>
2023-09-10 19:35:03 +0530

    second approach added

--- 6-sum-of-all-numbers.js
+++ 6-sum-of-all-numbers.js
@@ -3,3 +3,16 @@ const sumOfAllNumbers = (numbers) => {
     return numbers.reduce((acc, curr) => acc + curr);
 }
 console.log(sumOfAllNumbers([340, 243, 488, 983, 564, 231]));
+
+
+// second approach
+
+
+let sum = 0
+const totalOfAllNumbers = (num) => {
+    for (let i = 1; i <= num; i++) {
+       sum += i
+    }
+    return sum;
+}
+console.log(totalOfAllNumbers(10));
