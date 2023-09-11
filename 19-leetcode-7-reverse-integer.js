72f708bd6f56f77c6aaa89de79551f2af7d21f23
Ujef55 <ujef.kelawala123@gmail.com>
2023-09-12 01:04:06 +0530

    done

--- 19-leetcode-7-reverse-integer.js
+++ 19-leetcode-7-reverse-integer.js
@@ -1,13 +1,37 @@
+// const reverse = (int) => {
+
+//     let isNegative = false;
+//     if (int < 0) {
+//         isNegative = true;
+//     }
+
+//     if(isNegative) {
+//         int =+ ('-' + int.toString().split('').reverse().join(''));
+//     } else {
+//         int = +int.toString().split('').reverse().join('');
+//     }
+
+//     if (int > 2**31 - 1 || int < -1 * 2**31) {
+//         return 0
+//     }
+//     return int;
+// }
+
+// console.log(reverse(15342364));
+
+
 const reverse = (int) => {
-    let revInt = int.toString().split('').reverse().join('');
-    
-    if(revInt.endsWith('-')) {
-        revInt = '-' + revInt;
-        return parseInt(revInt);
-    } else {
-        return parseInt(revInt);
+    let sign = Math.sign(int);
+
+    let result = sign * String(Math.abs(int)).split('').reverse().join('');
+
+    let limit = Math.pow(2, 31);
+
+    if(result <= -limit || result > limit) {
+        return 0;
     }
     
-}
+    return result;
+};
 
-console.log(reverse(-789));
+console.log(reverse(123656576765));
