f353583ce7d76f2e00721d305b1dabb6d4ac3a02
Ujef55 <ujef.kelawala123@gmail.com>
2023-09-12 01:05:18 +0530

    done

--- 19-leetcode-7-reverse-integer.js
+++ 19-leetcode-7-reverse-integer.js
@@ -20,6 +20,12 @@
 // console.log(reverse(15342364));
 
 
+// Given a signed 32-bit integer x, 
+// return x with its digits reversed. 
+// If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], 
+// then return 0.
+
+
 const reverse = (int) => {
     let sign = Math.sign(int);
 
