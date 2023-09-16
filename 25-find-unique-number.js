a359b80ff1b1d955ffefae8f3fdefd10912981f8
Ujef55 <ujef.kelawala123@gmail.com>
2023-09-16 15:50:22 +0530

    done

---
+++ 25-find-unique-number.js
@@ -0,0 +1,9 @@
+const findUniqueNumber = (arr) => {
+    let ans = 0;
+    for (let i = 0; i <= arr.length; i++) {
+        ans = ans ^ arr[i];
+    }
+    return ans;
+}
+
+console.log(findUniqueNumber([1,2,1,6,4,2,4]));
