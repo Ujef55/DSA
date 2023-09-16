de0068654adec1bf794abef323bfbe6255f4f390
Ujef55 <ujef.kelawala123@gmail.com>
2023-09-17 05:12:03 +0530

    Added similar leetcode problem and solution

--- 27-find-duplicate-number.js
+++ 27-find-duplicate-number.js
@@ -17,3 +17,20 @@ const findDuplicate = (arr) => {
 }
 
 console.log(findDuplicate([1,3,2,4,5,2]));
+
+
+// Similar leetcode problem number-442 find all duplicates in an array
+
+const findDups = (numbers) => {
+    let mySet = new Set();
+    let dups = []
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (mySet.has(nums[i])) {
+            dups.push(nums[i]);
+        } else {
+            mySet.add(nums[i]);
+        }
+    }
+    return dups;
+}
