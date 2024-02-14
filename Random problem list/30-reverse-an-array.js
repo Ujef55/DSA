2815effa6f79189dfad49b51fd1e84b5fd2ee89a
Ujef55 <ujef.kelawala123@gmail.com>
2024-02-14 16:23:04 +0530

    Reverse an array with out using pre built reverse method.

---
+++ Random problem list/30-reverse-an-array.js
@@ -0,0 +1,27 @@
+// Given an array of integers and a position
+// you have to reverse an array after that position
+// with out using pre built reverse method.
+
+const reverseArray = (arr, m) => {
+    let start = 0;
+    let end = arr.length - 1;
+
+    for (let i = 0; i<end; i++) {
+        if (i === m) {
+            start = i;
+        }
+    }
+    
+    while (start<=end) {
+        let temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+    
+        start++
+        end--
+    }
+    return arr;
+    
+}
+
+console.log(reverseArray([11,7,3,12,4], 2));
